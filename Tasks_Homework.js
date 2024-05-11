0ac83002bcc7be1456ee365feb2ec938360c7ca3
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-05-11 23:27:12 +0300

    did ex.3

--- Tasks_Homework.js
+++ Tasks_Homework.js
@@ -4,14 +4,14 @@
 // Write a JavaScript function named calculateSquarePerimeter 
 // that takes one parameter: sideLength (the length of a side of the square).
 
-const sideLength = 5
+const sideLength = 5;
 
 function calculateSquarePerimeter(sideLength) {
 
 return 4*sideLength;
 }
 
-console.log(calculateSquarePerimeter(sideLength))
+console.log(calculateSquarePerimeter(sideLength));
 
 
 // ----------------------------------------------------
@@ -22,8 +22,28 @@ console.log(calculateSquarePerimeter(sideLength))
 // Define a JavaScript function named celsiusToFahrenheit that takes one parameter: 
 // celsius (the temperature in Celsius).
 
-
+const celsius = 100;
 function celsiusToFahrenheit(celsius) {
+    
     return (celsius * 9/5) + 32;}
     
-console.log(celsiusToFahrenheit(100))
+console.log(celsiusToFahrenheit(celsius));
+
+// --------------------------------------------------------------
+
+// Task 3: Create a function to calculate the area of a rectangle
+// Objective: To practice implementing a JavaScript function for computing the area of a rectangle.
+// Instructions:
+// Define a JavaScript function named calculateRectangleArea that takes two parameters:
+// length (the length of the rectangle) and width (the width of the rectangle).
+
+
+const length = 20;
+const width = 10;
+
+function calculateRectangleArea(length, width) {
+
+    return length*width;
+
+}
+console.log(calculateRectangleArea(length, width));

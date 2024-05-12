30a33ff6707a26e288c1871392620f0ee413d9b6
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-05-12 15:07:56 +0300

    undefined as a result when requesting an index that doesn't exis

--- 3rd lecture_excercises.js
+++ 3rd lecture_excercises.js
@@ -6,4 +6,11 @@ const s = "Hello World"
 // how many indices does the string have?
 // console.log(s.length-1)
 
-console.log(s[0])
+// index of 1 returns 2nd letter of the string
+// console.log(s[1])
+
+// how to select the last letter from the string
+// console.log(s[s.length-1])
+
+// we get undefined as a result when requesting an index that doesn't exist
+// console.log(s[109])

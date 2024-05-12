bb2a94aa97cdfdeea66955b17d735d5780d0c907
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-05-12 15:00:44 +0300

    index of 0

--- 3rd lecture_excercises.js
+++ 3rd lecture_excercises.js
@@ -1,2 +1,9 @@
 const s = "Hello World"
-console.log(s.length)
+
+// what is the length of the string?
+// console.log(s.length)
+
+// how many indices does the string have?
+// console.log(s.length-1)
+
+console.log(s[0])

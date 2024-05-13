cea2827af7056c0e5ffd38721ca30a9bbc363951
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-05-13 13:26:02 +0300

    concat

--- 3rd lecture_excercises.js
+++ 3rd lecture_excercises.js
@@ -47,5 +47,7 @@ const text= 'Cat, Dog, Mouse'
 
 const str1 = 'Hello'
 const str2 = 'World'
-console.log(str1 + " " + str2)
+// console.log(str1 + " " + str2)
+
+console.log(str1.concat(" ", str2, "!"))
 

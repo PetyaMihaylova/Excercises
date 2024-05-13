871601dfe6e7a83130d998c1bcb2ae297f0840c2
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-05-13 22:35:22 +0300

    text.indexOf()

--- 3rd lecture_excercises.js
+++ 3rd lecture_excercises.js
@@ -76,7 +76,7 @@ const s = "Hello World"
 // const text = 'Dog, Dog, Cat, Cat'
 // console.log(text.replaceAll('Dog','Cat'))
 
-const text = 'a b,c,d,e,f';
+// const text = 'a b,c,d,e,f';
 // console.log(text.split(","));
 // console.log(text.split(" "));  //  ['a', 'b,c,d,e,f']
 // console.log(text.split(''))        // ['a', ' ', 'b', ',', 'c', ',', 'd', ',', 'e', ',', 'f']
@@ -85,4 +85,7 @@ const text = 'a b,c,d,e,f';
 
 // const arr = [1, 'asd', 789]
 // const userArr = [Petya, Donal, Michael, Conall]
+
+// const text = 'Hello world at location Bulgaria';
+// console.log(text.indexOf('at')); //12
 

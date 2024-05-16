b67c74c32d28018cf98e995be63dd0f2a7e4be27
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-05-16 18:14:04 +0300

    function (condition) syntax

--- function_excercises_lecture4.js
+++ function_excercises_lecture4.js
@@ -7,6 +7,8 @@
 
 // console.log (solve(true)) //Hello
 
+// -----------------------------------------------------------
+
 // function solve(condition) {
 //     if (condition) {
 //  return ('Hello')
@@ -15,3 +17,43 @@
 // }
 
 // console.log (solve(false)) //undefined
+
+// -----------------------------------------------------------
+
+// function solve(number) {
+    
+//     if (number>0) {
+// return 'Greater than 0'
+//     }
+// else if(number<0) {
+//     return 'Less than 0'}
+// else if (number===0) {
+//     return 'Number equal to 0'
+// }
+
+//     else {return 'Not a valid number'}
+
+// }
+
+// console.log (solve(0))
+
+//---------------------------------------------------------
+
+function solve(number) {
+    let msg;
+    if (number>0) {
+msg= 'Greater than 0'
+    }
+ if(number<0) {
+    msg= 'Less than 0'}
+else if (number===0) {
+    msg= 'Number equal to 0'
+}
+
+    else {msg= 'Not a valid number'}
+return msg
+}
+
+console.log (solve(1))
+
+console.log(2==='2')

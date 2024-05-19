9e468958d9b82be43ffde9c1d7d4a9e5bac4c531
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-05-19 15:15:09 +0300

    task to print all letters in a string

---
+++ task_letters.js
@@ -0,0 +1,29 @@
+// function solve(string) {
+//     let n=0;
+    
+
+//     while (n < string.length) {
+       
+//         console.log(string[n]);  
+//         n++
+        
+//     }
+// }
+
+// solve('Congratulations')  
+
+
+function solve(string) {
+    let n=0;
+    
+
+    while (n < string.length) {
+       
+        console.log(string.charAt(n++));  
+        
+        
+    }
+}
+
+solve('Congratulations') 
+

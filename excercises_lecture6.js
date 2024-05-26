29e8188c6a6b11fb7582acd9a24df6bb9fea7917
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-05-26 23:13:00 +0300

    this is a function to check if a string is palindrome

---
+++ excercises_lecture6.js
@@ -0,0 +1,10 @@
+//check if a string is palindrome
+
+function solve(string){
+const fromStart = string
+const stringL = string.length
+let stringToCheck;
+for(let index = stringL-1; index >=0; index--); //we are starting from the back to forth, from the last index
+}
+
+console.log(solve('otto'))

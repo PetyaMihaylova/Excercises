0f6c7610d52c8a81c89f3cb2c2e773c2b542ff04
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-06 15:20:45 +0300

    //the map() method will run the callback function for each element of the array//

--- excercises_lecture9.js
+++ excercises_lecture9.js
@@ -34,17 +34,21 @@ const arr = ['Hello', 'World', 'Good', 'Bad']
 //     count++})
 //         console.log(count)
     
+// // }
+// // solve(arr)
+
+// function solve(arr){
+//     const newArr = []
+//     arr.forEach(element => newArr.push(element + '!')) //Because forEach does not return a result, 
+//     //there is a new method, which returns a new array, that is, it returns a result. it is called map
+//     console.log(newArr)
 // }
 // solve(arr)
-
-function solve(arr){
-    const newArr = []
-    arr.forEach(element => newArr.push(element + '!')) //Because forEach does not return a result, 
-    //there is a new method, which returns a new array, that is, it returns a result. it is called map
-    console.log(newArr)
-}
-solve(arr)
 
 // const newArray = arr.forEach(element => {return(element + '!')})
 // console.log(newArray) //we get undefined, because forEach()method does not have a return
 //We use an anonymous function when we do not have to re-use the logic
+
+const newArr = arr.map(element => element+ '!')
+
+console.log(newArr) //the map() method will run the callback function for each element of the array//(4) ['Hello!', 'World!', 'Good!', 'Bad!']

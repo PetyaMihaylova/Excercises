0207bf3fd3df1366c74a6777c32a3ded447010ad
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-06 13:27:49 +0300

    const arrowFunction = text => console.log(text) //if we have just one line of text in the body part, //we can remove {} and return. Similarly, if we have just 1 parameter in the (), we can remove the ()

---
+++ excercises_lecture9.js
@@ -0,0 +1,22 @@
+//solve()
+// function solve() {
+//     return('hello')
+// }
+
+// const mySolve = solve
+// console.log(mySolve())
+// const arrowFunction = () => {console.log ('hello')}
+// arrowFunction()
+
+// function solve(text) {
+//     return(text)
+// }
+
+// console.log(solve('hello 1'))
+
+// const arrowFunction = (text) => {console.log(text)}
+// arrowFunction('hello 2')
+
+const arrowFunction = text => console.log(text) //if we have just one line of text in the body part, 
+//we can remove {} and return. Similarly, if we have just 1 parameter in the (), we can remove the ()
+arrowFunction('hello 2')

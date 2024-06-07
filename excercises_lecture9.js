e2f333a753cb47e1b6a4d80fed61d3e2918e4f28
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-07 10:31:29 +0300

    //the arr.concat() method returns an array

--- excercises_lecture9.js
+++ excercises_lecture9.js
@@ -79,4 +79,4 @@ console.log(arr + arr1) //1,2,3,45,6,7,8 //if u just use the + sign, where the a
 console.log(arr + ',' + arr1) //1,2,3,4,5,6,7,8 //this is a string
 console.log(typeof(arr+arr1)) //string
 console.log(arr.concat(arr1)) // (8) ['1', '2', '3', '4', '5', '6', '7', '8']
-
+//the arr.concat() method returns an array

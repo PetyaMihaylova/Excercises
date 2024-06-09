35f777de54ddc06653b463e3d080eff737ccbf5f
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-09 15:31:39 +0300

    //with arr.forEach(), we find the last element fulfilling the condition //with arr.find(), we find the first element fulfilling the condition

--- excercise_lecture10.js
+++ excercise_lecture10.js
@@ -25,6 +25,9 @@
 
 //     console.log(result) //12 gives us the first element which fulfills the condition
 
-const array = [5, 12, 8, 130, 44]
-const result = array.find((element) => element > 10)
-console.log(result); //12, array.find() method returns us the first element abiding by the predicate. and undefined if no element abides
+// const array = [5, 12, 8, 130, 44]
+// const result = array.find((element) => element > 10)
+// console.log(result); //12, array.find() method returns us the first element abiding by the predicate. and undefined if no element abides
+
+//with arr.forEach(), we find the last element fulfilling the condition
+//with arr.find(), we find the first element fulfilling the condition

c67d871a8fb4429ed058157a479ffdfa31fc4fe8
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-09 15:23:58 +0300

    find() method returns us the first element abiding by the predicate. and undefined if no element abides

---
+++ excercise_lecture10.js
@@ -0,0 +1,30 @@
+// const array = [5, 12, 8, 130, 44]
+// let result;
+
+// array.forEach((element) => {
+    
+    // if (element >10 && haveHit) {
+    // result = element
+   
+    // }
+    // })
+
+    // console.log(result) //44 // gives us the last element of the array, fulfilling the condition
+
+    //------------------------------------
+// const array = [5, 12, 8, 130, 44]
+// let result;
+// let haveHit = true
+// array.forEach((element) => {
+    
+//     if (element >10 && haveHit) {
+//     result = element
+//     haveHit = false
+//     }
+//     })
+
+//     console.log(result) //12 gives us the first element which fulfills the condition
+
+const array = [5, 12, 8, 130, 44]
+const result = array.find((element) => element > 10)
+console.log(result); //12, array.find() method returns us the first element abiding by the predicate. and undefined if no element abides

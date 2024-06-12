f5bf110795bab166de3cdbff2441abe8c1249a03
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-12 23:35:54 +0300

    user.street = 'Ivan Vazov' //we can rewrite an object similarly to rewriting an array

--- excercises_lecture11.js
+++ excercises_lecture11.js
@@ -5,3 +5,10 @@ const user = {
     city: 'Sofia',
     level: 4
 }  //written in this way, the keys are always from type string
+
+// user = {} //This gives us an error, because we cannot rewrite something to a constant
+user.street = 'Ivan Vazov' //we can rewrite an object similarly to rewriting an array
+
+console.log(user.name) //Mitko
+console.log(user.age) //24
+console.log(user.street) //Ivan Vazov

7f959e073e5aec50d2ce5af4cbd8b9eea6a498a3
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-13 13:05:41 +0300

    //to check, whether an object has a certain property/key, //we can use a method on the global Object, namely Object.hasOwn(arr_name, 'property_name')

--- excercises_lecture11.js
+++ excercises_lecture11.js
@@ -27,10 +27,59 @@ const user = {
     }
 
 console.log(user.family) //(2) [{…}, {…}] //for a value in a key, we can have an array of objects
+console.log(user.family.find((member) => member.role === 'mother'))// {name: 'Polya', role: 'mother'}
 
-const key = 'name_first'
-console.log(user.name_first) //Mitko
-console.log(user['key']) //undefined
-console.log(user[key])  ///Mitko Here we can dynamically take a value from the outside, like key
-user[key] = 'Ivan'
-console.log(user[key])
+// const key = 'name_first'
+// console.log(user.name_first) //Mitko
+// console.log(user['key']) //undefined
+// console.log(user[key])  ///Mitko Here we can dynamically take a value from the outside, like key
+// user[key] = 'Ivan'
+// console.log(user[key])
+
+
+for (property in user){
+    console.log(property
+
+    )
+}
+// name_first
+// email
+// age
+// city
+// level
+// family
+
+// for(key in object_name){
+//     console.log(key)
+// }
+
+for (let property of user.family){
+    console.log(property
+
+    )} //{name: 'Polya', role: 'mother'}
+    // {name: 'Dancho', role: 'father'} //this is the for of loop in arrays
+
+    //in arrays, we have for of loop
+    //in objects, we have for in loop
+
+for(property in user){
+    console.log(user[property])
+} //
+// Mitko
+// mitko@abv.bg
+// 24
+// Sofia
+// 4
+// (2) [{…}, {…}]
+
+for(property in user){
+    console.log(property === 'name_first', property)
+} // true name_first
+// false email
+// false age
+// false city
+// false level
+// false family
+
+Object.hasOwn(user, 'level') //to check, whether an object has a certain property/key, 
+//we can use a method on the global Object, namely Object.hasOwn(arr_name, 'property_name')

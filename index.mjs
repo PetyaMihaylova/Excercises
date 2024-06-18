61590f08944ad6eb3fad1898421d9f7823aaebdf
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-18 23:53:54 +0300

    moved all 5 files to this workspace

---
+++ index.mjs
@@ -0,0 +1,32 @@
+// const obj = [{
+// "name": "Muhammad Ali",
+// "gender": "male",
+// "age": 23
+// },
+// {
+//     "name": "Muhammad Ali",
+// "gender": "male",
+// "age": 23
+// }]
+// const jasonObject = JSON.stringify(obj)
+// const parseJSON = JSON.parse(jasonObject)
+// console.log(typeof jasonObject)//string
+// console.log(typeof parseJSON)//object
+
+//we can stringify object or array
+
+// import moment from 'moment/moment.js';
+
+// const moment = require('moment'); // require
+// console.log(moment().get('Year')) //2023
+// console.log(moment().calendar)
+// console.log(moment)
+// console.log(moment().format('MMMM Do YYYY, h:mm:ss a')); //June 18th 2024, 2:55:27 pm
+// console.log(moment().format('ll'));  
+
+// const date = new Date() //in JavaScript, we have a special object that is responsible for the creation of dates
+// console.log(date) //Tue Jun 18 2024 13:23:58 GMT+0300 (Eastern European Summer Time)
+// const r = moment(moment().utc().format('YYYY-MM-DD HH:mm:ss')).toDate()
+// console.log(r) //Tue Jun 18 2024 11:34:35 GMT+0300 (Eastern European Summer Time)
+import  { asd } from './test.mjs';
+asd('hello from js modules')  //hello from js modules
---
+++ test.mjs
@@ -0,0 +1,16 @@
+// export const solve = (string) => console.log(string);
+// export const solve1 = (string) => console.log(string);
+// export const solve2 = (string) => console.log(string)
+
+// test = {
+// solve:(string) => console.log(string),
+// solve1:(string) => console.log(string),
+// solve2:(string) => console.log(string),
+// }
+
+// const {solve, solve2} = test
+
+//----------------------------------------
+
+const solve = (string) => console.log(string);
+export default solve

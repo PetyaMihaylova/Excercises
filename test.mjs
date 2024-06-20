6f22f199830a01ab2b5fa644062e44a26dcf447d
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-20 13:26:55 +0300

    .split() method is typical of strings and arrays, nto applicable to objects

--- test.mjs
+++ test.mjs
@@ -12,5 +12,42 @@
 
 //----------------------------------------
 
-const solve = (string) => console.log(string);
-export default solve
+// const solve = (string) => console.log(string);
+// export default solve
+
+//---------------------------------------
+
+// const now = new Date()
+// const now = new Date(1989, 2, 18, 11, 30, 0)// year, month, day, hour, minutes, seconds
+// console.log(now)// Wed Jun 19 2024 00:17:49 GMT+0300 (Eastern European Summer Time)// if we do not add any parameters in const now = new Date(), it gives us the current date
+//the ISO standard for dates is called ISO 8601
+// const isoDate = '2024-02-18T11:30:00'
+// const now = new Date(isoDate) //Sun Feb 18 2024 11:30:00 GMT+0200 (Eastern European Standard Time)
+// console.log(now)
+
+
+const isoDate = 1700000000000000 //Mon Nov 09 55840 00:13:20 GMT+0200 (Eastern European Standard Time)
+const now = new Date(isoDate) //Sun Feb 18 2024 11:30:00 GMT+0200 (Eastern European Standard Time)
+console.log(now.getFullYear())//55840
+console.log(now.getDate());//9
+console.log(now.getDay());//1// getDay() method returns the day as an indexed value. That is, Sunday is 0, Monday is 1, etc.
+console.log(now.setFullYear(1989))
+console.log(now); //Thu Nov 09 1989 00:13:20 GMT+0300 (Eastern European Standard Time)
+console.log(now.setMonth(11))
+console.log(now); //Sat Dec 09 1989 00:13:20 GMT+0300 (Eastern European Standard Time)
+//here, the months are also indexed. 0 is january, 11 is december
+console.log(now.setDate(25))
+console.log(now);//C:\Program Files\nodejs\node.exe .\test.mjs
+console.log(now.setHours(11));
+console.log(now)//Mon Dec 25 1989 11:13:20 GMT+0300 (Eastern European Standard Time)
+console.log(typeof now) //object
+
+//with a get method, we obtain something, with a set method, we set something
+//Obects do not have the .split() method -- only strings and arrays have it.
+
+55840
+9
+1
+
+// Mon Dec 25 1989 00:13:20 GMT+0300 (Eastern European Standard Time)
+

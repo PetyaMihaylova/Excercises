837e4526d91e50b49eb3cbff647957a8c5a65483
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-06-22 16:09:57 +0300

    //Getters can also have conditions. E.g., upon the fulfillment of a certain condition, we can get particular information

--- excercises_lecture12.mjs
+++ excercises_lecture12.mjs
@@ -73,6 +73,7 @@ const car2 = new Object({
 // new Object{}
 
 class Car {
+    #id = '1234'
     constructor(brand, model, year){
         this.brand = brand; //here, we are constructing key-value pairs with this. HOwever, here, the key-value relationship is represented by an equal = sign, not by :
         this.model = model;
@@ -90,21 +91,55 @@ else {return 0}
 
 get info() {  //Getters are like any other method. However, they give us the opportunity to receive info from the object
     return `${this.brand}, ${this.model}, ${this.year}`
+
+}
+get id() {
+    return this.#id
+}
+set id(x) {
+    if(typeof x === 'string' && x.length === 4){
+
+    
+    this.#id = x} //The Setter allows us to set a new id. Also, the Setter allows us to create conditional logic for setting of certain things inside the object we created
+else {console.error('Id must be a string and be exactly 4 symbols')}
+
 }
 }
 
+
 const myCar = new Car('VW', 'Polo', 2018) //2000
 const yourCar = new Car('Toyota', 'Master', 2018)//1000
-myCar.info = 2 //"ncaught TypeError TypeError: Cannot set property info of #<Car> which has only a getter." Here, we cannot set a property to the object by calling the object and its method with a dot,
+yourCar.id = 1234 //We get thrown the following error: "Id must be a string and be exactly 4 symbols"
+// myCar.info = 2 //"ncaught TypeError TypeError: Cannot set property info of #<Car> which has only a getter." Here, we cannot set a property to the object by calling the object and its method with a dot,
 //the same way we change key-value pairs in an object, because, with teh Get methods, we only get
 //information, we cannot set it. In that sense, the Getter is not exactly key: value pair in an object
 
 // console.log(myCar.checkPrice());
 // console.log(yourCar.checkPrice())
-console.log(myCar.info); //VWPolo2018
+// console.log(myCar.info); //VWPolo2018
+// yourCar.id = '4321'
+// console.log(yourCar.id);
+// console.log(myCar.id);
+
+// console.log(myCar.id)//1234
 
 //a function, which is either in the class or the object, is called a method
 
 
 // console.log(myCar);// Car {brand: 'Volkswagen', model: 'Polo', year: 2018}
 
+// class Button {
+// constructor (text, onClick, color){
+//     this.text = text;
+//     this.color = color;
+    
+// }
+// onClick(){
+        
+// }
+// }
+
+// const obj{id: '1234'}
+// obj.id = 4321
+
+//Getters can also have conditions. E.g., upon the fulfillment of a certain condition, we can get particular information

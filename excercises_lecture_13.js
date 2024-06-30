a9242cbb11f07cc4b8ebfe914cbb501d35ff237d
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-07-01 01:24:36 +0300

    by putting a function inside a function, we achieve the concept of closure, which allows us to preserve state of a private variable, without exposing it to the global scope

--- excercises_lecture_13.js
+++ excercises_lecture_13.js
@@ -1,12 +1,22 @@
 
-function outerFunction () {
-    let count = 0
-    function increaseCount(){
-    count = count + 1
-}
-    return count
-}
+// function outerFunction () {
+//     let count = 0
+//     function innerFunction(){
+//     count = count + 1
+//     console.log(count);
+// }
+//     return innerFunction
+// }
 
-console.log(outerFunction())
-console.log(outerFunction())
-console.log(outerFunction())
+// // console.log(outerFunction())
+// // console.log(outerFunction())
+// // console.log(outerFunction())
+
+// const countHandler = outerFunction()
+// countHandler()
+// countHandler()
+// countHandler()
+
+//-----------------------------------------------------------
+
+const createCounter = () => {}

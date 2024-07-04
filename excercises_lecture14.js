5e0ffbb7520e48f403dd91e7f1f5f1f5af15eee8
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-07-04 23:24:45 +0300

    querySelector(). //querySelector() can select elements by tag name, by class name and by id.  //When we use the tag name, we always select the first element that is a match for the tag.

--- excercises_lecture14.js
+++ excercises_lecture14.js
@@ -1,15 +1,23 @@
 const app = () => {
     const myDiv = document.getElementById('myDiv')
-    console.log({myDiv});
+    // console.log({myDiv});
     const allDivs = Array.from(document.getElementsByTagName('div')) 
-    console.log(allDivs);
+    // console.log(allDivs);
     const myElementsByClassName = Array.from(document.getElementsByClassName('MyClass'))
-    console.log({myElementsByClassName});
+    // console.log({myElementsByClassName});
+
+    //querySelector() can select elements by tag name, by class name and by id. 
+    //When we use the tag name, we always select the first element that is a match for the tag.
+    const myDivQ = document.querySelector('div')
+    myDiv.style.backgroundColor = 'green'
+    console.log({myDivQ});
+
     // console.log(allDivs.length)
     // console.log(allDivs.item(0))
     // console.log(allDivs[0])
     // console.log(allDivs[allDivs.length-1])
 // console.log(allDivs.forEach((e) => console.log(e)))
+
 console.log(allDivs.forEach((div, index) => div.innerText = index))
 myElementsByClassName.forEach((element, index) => element.style.backgroundColor = 'red')
 }
@@ -21,4 +29,7 @@ window.addEventListener('load', app)
 //jQuery allows us to select elements in the same way that we select elements in CSS.
 //In time, JQuery had a huge success and because of that, it has been integrated in JavaScript, 
 //so there is no need to install JQuery separately.
+
+//The way we write in CSS with the dashes "-" is called "kebab case." In JavaScript, we do not use
+//"kebab case", we use "camel case", where the 2nd letter is capitalized.
 

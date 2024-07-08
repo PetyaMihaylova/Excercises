b04551a768e6ad836e30504e27225986f987366b
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-07-08 11:09:05 +0300

    we created constants with the HTML elements from the form, so that each element is selected by means of JavaScript with the help of the .getElementById() method, rendering an output of an HTML Collection each time. We could transform the HTML Collection into an array each time, should we need to do so in the code later.

---
+++ excercices_lecture16.js
@@ -0,0 +1,9 @@
+const app = () => {
+const form = document.getElementById('form')
+const nameInput = document.getElementById('name')
+const emailInput = document.getElementById('email')
+const messageInput = document.getElementById('message')
+const submitButton = document.getElementById('button')
+}
+
+window.addEventListener('load', app)

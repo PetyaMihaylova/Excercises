5d36064cb1800a5c773ee3b4a13fc0d52cb02a42
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-07-08 11:29:21 +0300

    //Every single event handler that we add to the Event Listener comes with an object called event. //This object comes with its own methods. Generally, the default behavior of an HTML form after //pressing the button 'submit' is to reload the page and clear everything that has been written //in the form. However, in order to prevent the clearing of the information after the reload, //we can add event.preventDefault() method in the event Handler function, in this case called //submitHandler().

--- excercices_lecture16.js
+++ excercices_lecture16.js
@@ -4,6 +4,21 @@ const nameInput = document.getElementById('name')
 const emailInput = document.getElementById('email')
 const messageInput = document.getElementById('message')
 const submitButton = document.getElementById('button')
+
+console.log(form, nameInput, emailInput, messageInput, submitButton);
+
+const submitHandler = (event) => {
+    event.preventDefault()
+}
+
+submitButton.addEventListener('click', submitHandler)
 }
 
 window.addEventListener('load', app)
+
+//Every single event handler that we add to the Event Listener comes with an object called event. 
+//This object comes with its own methods. Generally, the default behavior of an HTML form after
+//pressing the button 'submit' is to reload the page and clear everything that has been written
+//in the form. However, in order to prevent the clearing of the information after the reload, 
+//we can add event.preventDefault() method in the event Handler function, in this case called
+//submitHandler().

efdc1b47cdd93c593287246b1985e0e6469a2ce8
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-07-08 12:45:36 +0300

    //When we write if(nameValue && emailValue && messageValue), we mean that if these constants are not empty, //otherwise, when they are empty strings or just empty, the value inside the if-statement will be falsy, //hence the body of the function will not be run!

--- excercices_lecture16.js
+++ excercices_lecture16.js
@@ -9,6 +9,15 @@ console.log(form, nameInput, emailInput, messageInput, submitButton);
 
 const submitHandler = (event) => {
     event.preventDefault()
+    const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
+    if(nameValue && emailValue && messageValue) {
+        alert (`Your name from ${nameValue} with email ${emailValue} and Message ${messageValue}`)
+ 
+    } else {
+        alert('Please fill all inputs!')
+    }
 }
 
 submitButton.addEventListener('click', submitHandler)
@@ -22,3 +31,10 @@ window.addEventListener('load', app)
 //in the form. However, in order to prevent the clearing of the information after the reload, 
 //we can add event.preventDefault() method in the event Handler function, in this case called
 //submitHandler().
+
+//In this case, refreshing of the page is like refreshing our applications. In some cases we may
+//want to do that, but in others, we will not want to do that.
+
+//When we write if(nameValue && emailValue && messageValue), we mean that if these constants are not empty,
+//otherwise, when they are empty strings or just empty, the value inside the if-statement will be falsy,
+//hence the body of the function will not be run!

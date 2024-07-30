52bb09efc676d1208296ff71019db7ac5989e831
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-07-30 10:37:38 +0300

    installed node modules and started working to create simple applications in react

---
+++ my-app/src/components/header-and-button.jsx
@@ -0,0 +1,14 @@
+import Header from "./header"
+import Button
+ from "./button"
+
+const HandB = () => {
+    return (
+    <>
+    <Header />
+    <Button />
+    </>
+);
+}
+
+export default HandB
---
+++ my-app/src/components/header.jsx
@@ -0,0 +1,6 @@
+const Header = () => {
+    const text = 'My First Component'
+    return <h1 style ={{color: 'red'}}>{text}</h1>
+}
+
+export default Header
